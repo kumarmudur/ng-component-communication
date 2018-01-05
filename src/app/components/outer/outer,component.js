5d868ccdfa5e090fc343a453e0fe81c3a9bbe17c
kumarmudur <30613897+kumarmudur@users.noreply.github.com>
2018-01-05 17:40:46 +0530

    Nothing use

--- src/app/components/outer/outer,component.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  'outer': {
-    'border': [{ 'unit': 'px', 'value': 1 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': 'green' }],
-    'padding': [{ 'unit': 'px', 'value': 5 }, { 'unit': 'px', 'value': 5 }, { 'unit': 'px', 'value': 5 }, { 'unit': 'px', 'value': 5 }]
-  }
-});
