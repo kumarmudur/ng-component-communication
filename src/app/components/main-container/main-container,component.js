dd74821c180902a39294c3096f814c20da3e914b
kumarmudur <30613897+kumarmudur@users.noreply.github.com>
2018-01-05 17:41:12 +0530

    nothing use

--- src/app/components/main-container/main-container,component.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  'main-container': {
-    'border': [{ 'unit': 'px', 'value': 1 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': 'blue' }],
-    'padding': [{ 'unit': 'px', 'value': 5 }, { 'unit': 'px', 'value': 5 }, { 'unit': 'px', 'value': 5 }, { 'unit': 'px', 'value': 5 }]
-  }
-});
